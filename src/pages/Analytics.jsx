540b63af0794ff4b64fdb365390716c3956e6974
shivani1031 <69902161+shivani1031@users.noreply.github.com>
2023-06-08 17:39:21 +0530

    Update Analytics.jsx

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+    <div>
+<h1>Analytics</h1>
+<br />
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Error adipisci animi debitis a maiores mollitia dolorum nulla, 
+  dolorem odio in autem voluptatem! Id magnam deserunt dolores 
+  consequuntur ea? Blanditiis suscipit exercitationem eaque sapiente. 
+  Quod dolorum at nisi voluptas repellendus nam, deleniti tempora illo
+  consectetur, rerum delectus ipsa ipsum quam debitis.
+</p>
+    </div>
   )
 }
 
