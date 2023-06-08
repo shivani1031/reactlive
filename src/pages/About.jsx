6fa6194f2c53a488abe00410fb413af0566f2b4a
shivani1031 <69902161+shivani1031@users.noreply.github.com>
2023-06-08 17:38:58 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Abodsdgut</div>
+    <div>
+
+      <h1>About</h1>
+      <br />
+      <p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Error adipisci animi debitis a maiores mollitia dolorum nulla, 
+  dolorem odio in autem voluptatem! Id magnam deserunt dolores 
+  consequuntur ea? Blanditiis suscipit exercitationem eaque sapiente. 
+  Quod dolorum at nisi voluptas repellendus nam, deleniti tempora illo
+  consectetur, rerum delectus ipsa ipsum quam debitis.
+</p>
+    </div>
   )
 }
 
